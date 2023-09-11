d29728c442423f618db3ce2fc8498f30fc6c1df7
Charlie Salcedo <salcedo.char0311@gmail.com>
2023-09-11 01:20:29 -0400

    lab doneexit

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+// Function to return the first two drivers from the array
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  // Function to return the last two drivers from the array
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  // Array containing the functions for selecting drivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Function to create a fare multiplier
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  // Function to double the fare
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Function to triple the fare
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different drivers based on a given function
+  function selectDifferentDrivers(drivers, driverSelectionFunction) {
+    return driverSelectionFunction(drivers);
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers,
+  };
+  
